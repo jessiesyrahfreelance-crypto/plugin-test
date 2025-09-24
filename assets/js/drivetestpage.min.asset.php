eac183e3d86bb6f5ee6527a79a02c15638f52617
Syrah Freelance <jessiesyrahfreelance@gmail.com>
2025-09-24 23:03:40 +0800

    Fixed the npm commands

--- assets/js/drivetestpage.min.asset.php
+++ assets/js/drivetestpage.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-components', 'wp-element'), 'version' => '23823093e41dcb06f581');
+<?php return array('dependencies' => array('wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b6f849a6a06ffd18a6ee');
